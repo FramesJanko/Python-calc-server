9bdcb1599099c08cb3c6d4168a6f38e7d58425c3
FramesJanko <rheckman4143@gmail.com>
2024-07-08 01:55:28 -0400

    stuff that works

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import calculator
 
 app = Flask(__name__)
 
-variable = 'test'
 email = 'rheckman4143@gmail.com'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/ContactMe')
-def contact():
-    return f'<h1>Contact Me at {email}</h1>'
+@app.route('/submit', methods = ['POST'] )
+def submit():
+    if request.method == 'POST':
+        calculation = calculator.calculate(request.form["calculation"])
+        return (f'<div class="answer">{calculation}</div>')
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
