b9dcc8264e533011208cf75af665c171b91abb92
FramesJanko <rheckman4143@gmail.com>
2024-07-15 02:31:28 -0400

    Committing lots of style changes

--- server.py
+++ server.py
@@ -6,12 +6,14 @@
 #   - It should have an about section
 #   - My projects and proficiencies
 #   - Emulate a website that I enjoy, like Udemy, youtube, or twitch.
-from flask import Flask, render_template, request
+from flask import Flask, render_template, render_template_string, request
 import calculator
 
 app = Flask(__name__)
 
 email = 'rheckman4143@gmail.com'
+
+calc_history = []
 
 @app.route('/')
 def index():
@@ -21,7 +23,20 @@ def index():
 def submit():
     if request.method == 'POST':
         calculation = calculator.calculate(request.form["calculation"])
-        return (f'<div class="answer">{calculation}</div>')
+        calc_history.append(calculation)
+        html_response = """
+        <div class ="response">
+            <div class="answer">{{ calculation }}</div>
+            <div class="history">
+                <ul>
+                {% for item in calc_history %}
+                    <li>{{ item }}</li>
+                {% endfor %}
+                </ul>
+            </div>
+        </div>
+        """
+        return render_template_string(html_response, calculation=calculation, calc_history=calc_history)
     else:
         return render_template('index.html')
 
