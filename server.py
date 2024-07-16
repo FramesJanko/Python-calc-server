1f9c8d914a5c733e420f3f2377aa91cdaaa34628
FramesJanko <rheckman4143@gmail.com>
2024-07-16 09:36:59 -0400

    added first routing

--- server.py
+++ server.py
@@ -19,26 +19,30 @@ calc_history = []
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods = ['POST'] )
-def submit():
-    if request.method == 'POST':
-        calculation = calculator.calculate(request.form["calculation"])
-        calc_history.append(calculation)
-        html_response = """
-        <div class ="response">
-            <div class="answer">{{ calculation }}</div>
-            <div class="history">
-                <ul>
-                {% for item in calc_history %}
-                    <li>{{ item }}</li>
-                {% endfor %}
-                </ul>
-            </div>
-        </div>
-        """
-        return render_template_string(html_response, calculation=calculation, calc_history=calc_history)
-    else:
-        return render_template('index.html')
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
+
+# @app.route('/submit', methods = ['POST'] )
+# def submit():
+#     if request.method == 'POST':
+#         calculation = calculator.calculate(request.form["calculation"])
+#         calc_history.append(calculation)
+#         html_response = """
+#         <div class ="response">
+#             <div class="answer">{{ calculation }}</div>
+#             <div class="history">
+#                 <ul>
+#                 {% for item in calc_history %}
+#                     <li>{{ item }}</li>
+#                 {% endfor %}
+#                 </ul>
+#             </div>
+#         </div>
+#         """
+#         return render_template_string(html_response, calculation=calculation, calc_history=calc_history)
+#     else:
+#         return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
